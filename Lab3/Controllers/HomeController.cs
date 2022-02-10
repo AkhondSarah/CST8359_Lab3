52b6186f4d973d9e5b8cda641776556650c24b16
AkhondSarah <akhond.sarah@gmail.com>
2022-02-10 15:39:14 -0500

    Commit

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult SongForm() => View();
+
+        [HttpPost]
+        public IActionResult Sing()
+        {
+            // you will complete this
+            ViewData["noofbottles"] = Request.Form["noofbottles"];
+
+            return View();
+        }
+
+        public IActionResult CreatePerson() => View();
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Person person)
+        {
+            return View(person);
+        }
+        public IActionResult Error()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Lab3/Models/Person.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class Person
+    {
+        [Required]
+        [StringLength(100)]
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(100)]
+        public string LastName
+        {
+            get;
+            set;
+        }
+        public string Age
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(100)]
+        [EmailAddress]
+        public string EmailAddress
+        {
+            get;
+            set;
+        }
+
+
+        [Required]
+        [StringLength(100)]
+        public string Password
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(100)]
+        public string Description
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
